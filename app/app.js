762444d216990aa542e25c8deb6a9853c1db7199
juha-kantola <99681651+juha-kantola@users.noreply.github.com>
2022-11-23 12:25:25 +0200

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,18 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts";
+
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
+
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
+};
+
+const data = {};
+
+const handleRequest = async (request) => {
+  return new Response(await renderFile("index.eta", data), responseDetails);
+};
+
+serve(handleRequest, { port: 7777 });
